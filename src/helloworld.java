00fecc2e4df0701f6d094cd4faca77a196cd17b1
Benjamin Asj <s333933@oslomet.no>
2020-08-17 12:36:33 +0200

    Initial commit

---
+++ src/helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+    public static void main (String[] args){
+        System.out.println("Hello world");
+
+        System.out.println("Teller til 10");
+        for (int i=1; i<=10; ++i){
+        System.out.println(i);
+        }
+    }
+}
